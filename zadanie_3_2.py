67b7abecb35d850919a3b00dc854a5a1abbf5ceb
Agata StadnickaKwiecien <agata.stadnicka@gmail.com>
2025-03-19 13:56:40 +0100

    Update zadanie_3_2.py

---
+++ zadanie_3_2.py
@@ -0,0 +1,11 @@
+print("Zadanie 1")
+print("Lista zakupów")
+shopping_dict = {
+    "piekarnia" : ["chleb", "bułki", "pączek"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
+for shop, products in shopping_dict.items():
+    products = [product.capitalize () for product in products]
+    print(f"Idę do {shop.capitalize()} i kupuję tam: {products}.")
+total = sum(len(products) for products in shopping_dict.values())
+print("W sumie kupuję", total, "produktów.")
